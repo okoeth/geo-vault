a12efdc2d6e625a6e5e081abab962f454991d4c3
Darren <fieroazul@gmail.com>
2015-01-13 18:55:07 +0800

    Signed-off-by: Darren <fieroazul@gmail.com>

--- app.js
+++ app.js
@@ -30,35 +30,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-/*
-var locationsById = {
-    "123" : { id: "123", longitude: "48", latitude: "52" },
-    "124" : { id: "124", longitude: "32", latitude: "34" },
-    "125" : { id: "125", longitude: "62", latitude: "22" }
-  };
-*/
-
 var locationsById = null;
 var POIsByVid = null;
 
 app.get('/', function (req, res) {
-  res.render('index','Hello Vault');
-});
-
-app.post('/location', function (req, res) {
-	logger.info(req.body);
-	logger.info('POST: /location');
-	locationsById[req.body.id] = {
-        id : req.body.id,
-        longitude : req.body.longitude,
-        latitude : req.body.latitude,
-        createdTime : new Date()
-	};
-    console.log(locationsById)
-  logger.info('DATA: >>Before write');
-  fs.writeFileSync('database.json', JSON.stringify(locationsById));
-  logger.info('DATA: >>After write');
-  res.send({result:'okay'});
+    res.render("index","hello");
 });
 
 app.get('/location', function (req, res) {
@@ -74,31 +50,39 @@ app.get('/location', function (req, res) {
   callback_val = req.param('callback');
   res.end(callback_val+'('+JSON.stringify(locationsById[req.param('id')])+')')
 });
-
+app.get('/locations', function (req, res) {
+    logger.info('GET: /locations');
+    callback_val = req.param('callback');
+    res.end(callback_val+'('+JSON.stringify(locationsById)+');')
+});
 app.get('/location/:id', function (req, res) {
-	logger.info('GET: /location:id');
-	id_val = req.param('id');
+    logger.info('GET: /location:id');
+    id_val = req.param('id');
     callback_val = req.param('callback');
     res.end(callback_val+'('+JSON.stringify(locationsById[id_val])+')')
 });
+app.post('/location', function (req, res) {
+    logger.info(req.body);
+    logger.info('POST: /location');
+    locationsById[req.body.id] = {
+        id : req.body.id,
+        longitude : req.body.longitude,
+        latitude : req.body.latitude,
+        createdTime : new Date()
+    };
+    console.log(locationsById)
+    logger.info('DATA: >>Before write');
+    fs.writeFileSync('database.json', JSON.stringify(locationsById));
+    logger.info('DATA: >>After write');
+    res.send({result:'okay'});
+});
+
 
 app.get('/call/:id', function (req, res) {
     logger.info('GET: /call:id');
     id_val = req.param('id');
     callback_val = req.param('callback');
     res.end(callback_val+'('+JSON.stringify(CallsByCid[id_val])+')')
-});
-
-app.get('/locations', function (req, res) {
-	logger.info('GET: /locations');
-	callback_val = req.param('callback');
-  res.end(callback_val+'('+JSON.stringify(locationsById)+');')
-});
-
-app.get('/gateway', function (req, res) {
-  logger.info('GET: /gateway');
-  callback_val = req.param('callback');
-  res.end(callback_val+'('+"{result:'okay'}"+');')
 });
 
 // the service console can get store the POI information by this service  in the gateway
@@ -109,23 +93,118 @@ app.get('/poi/:id',function(req,res){
     id_val   = req.param('id');
     callback_val = req.param('callback');
 
-    var poi = {};
-    poi.title  = req.param('title').trim();
-    poi.address = req.param('address').trim();
-    poi.longitude = req.param('lng').trim();
-    poi.latitude = req.param('lat').trim();
+    console.log(req);
+
+    var arrLength = POIsByVid['poi'].length;
+    POIsByVid['poi'][arrLength] = {};
+
+
+    POIsByVid['poi'][arrLength].vin = req.param('vin').trim();
+    POIsByVid['poi'][arrLength].callid = '';
+    POIsByVid['poi'][arrLength].title = req.param('title').trim();
+    POIsByVid['poi'][arrLength].address = req.param('address').trim();
+    POIsByVid['poi'][arrLength].longitude = req.param('lng').trim();
+    POIsByVid['poi'][arrLength].latitude = req.param('lat').trim();
+    POIsByVid['poi'][arrLength].datetime = new Date();
 
     var result = {};
     result.resultCode = 0;
     result.resultDesc = '';
-    result.poi = poi ;
-    console.log(JSON.stringify(result));
 
     logger.info('DATA: >>Before write');
-    fs.writeFileSync('poi-database.json', JSON.stringify(result));
+    fs.writeFileSync('poi-database.json', JSON.stringify(POIsByVid));
     logger.info('DATA: >>After write');
     res.end(callback_val +'(' + JSON.stringify(result) + ')' );
 })
+
+// when cc closed the call, the service console will invoke this interface to finalize items below:
+//  1- pass in vid and callid;
+//  2- lookup all the vid related POI record;
+//  3- find out all the callid null valued record, set the callid value to all these records
+
+//  -  input json:{id:"",vid: ""}
+//     -> id means call id
+//     -> vid means the vehicle id
+//  -  output parameter: {resultCode:0,resultDesc:''}
+app.get('/poi-call/:id',function(req,res){
+    logger.info('GET: /poi-call:id');
+    id_val   = req.param('id');
+    callback_val = req.param('callback');
+
+    var vid = req.param('vid');
+    var callid = req.param('callid');
+
+    for (var poiCount = 0;poiCount < POIsByVid['poi'].length;poiCount++){
+        if(POIsByVid['poi'][poiCount].vid == vid && POIsByVid['poi'][poiCount].callid == ''){
+            POIsByVid['poi'][poiCount].callid = callid;
+        }
+    }
+
+    var result = {};
+    result.resultCode = 0;
+    result.resultDesc = '';
+
+    logger.info('DATA: >>Before write');
+    fs.writeFileSync('poi-database.json', JSON.stringify(POIsByVid));
+    logger.info('DATA: >>After write');
+    res.end(callback_val +'(' + JSON.stringify(result) + ')' );
+})
+
+app.get("/log",function(req,res){
+    var options = {
+        from: new Date - 10 * 24 * 60 * 60 * 1000,
+        until: new Date,
+        limit: 100,
+        start: 0,
+        order: 'desc',
+        fields: ['level','message','timestamp']
+    };
+
+    logger.query(options, function (err, results) {
+        if (err) {
+            throw err;
+        }
+        /*var files = results.file;
+        console.log(files);*/
+        res.render('log',{files:results.file});
+    });
+})
+
+app.get("/data",function(req,res){
+    var locations  = [];
+    var POIs = [];
+    var arrLocCount = 0;
+    for(var key in locationsById){
+        locations[arrLocCount] = locationsById[key];
+        arrLocCount ++;
+    }
+
+    var arrCallLocCount = 0;
+    var callLocations = [];
+    for(var key in CallsByCid){
+        for(var locCount =0;locCount < CallsByCid[key].length; locCount ++){
+            CallsByCid[key][locCount].token = key;
+            callLocations[arrCallLocCount] = CallsByCid[key][locCount];
+            arrCallLocCount++;
+        }
+    }
+
+    var arrPOIsLocCount = 0;
+    var poiLocations = [];
+
+    for(var poiCount = 0;poiCount <  POIsByVid['poi'].length; poiCount ++){
+        poiLocations[arrPOIsLocCount] = POIsByVid['poi'][poiCount];
+        arrPOIsLocCount++;
+    }
+    res.render('data',{locations:locations,callLocations:callLocations,poiLocations:poiLocations});
+})
+
+app.get('/gateway', function (req, res) {
+  logger.info('GET: /gateway');
+  callback_val = req.param('callback');
+  res.end(callback_val+'('+"{result:'okay'}"+');')
+});
+
 
 https.createServer(options, app).listen(443,function () {
     locationsById=JSON.parse(fs.readFileSync('database.json'));
--- public/js/index.js
+++ public/js/index.js
@@ -1,18 +1,19 @@
+/*
  alert("post started.")
- 
+
  $.ajax({
-	type:"post",
-	url:"/location",
-	data:{id:113292,longitude:13.1,latitude:26.5},
-	dataType:"json",
-	error: function(xhr,txtStatus,errThrow) {
-           alert("error:\n " + errThrow);
-        },
-	success:function(data){
-		if(data != null) {
-			alert(data);
-		} else {
-			showinfo(index,t);
-		}
-	}
-}); 
+ type:"post",
+ url:"/location",
+ data:{id:113292,longitude:13.1,latitude:26.5},
+ dataType:"json",
+ error: function(xhr,txtStatus,errThrow) {
+ alert("error:\n " + errThrow);
+ },
+ success:function(data){
+ if(data != null) {
+ alert(data);
+ } else {
+ showinfo(index,t);
+ }
+ }
+ }); */
